a7e3fe6d36528369522976120bab8eeb4918830a
briantmorr <briantmorr@gmail.com>
2018-03-02 10:43:23 -0800

    main mole array created

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ $(document).ready(initializeApp);
 var whackMole = null;
 var moles = [];
 for(var i = 0; i < 9; i++){
-    
+    var mole = {
+        name: i
+    }
+    moles.push(mole);
 }
 function initializeApp(){
     whackMole = new MoleGame(moles);
     applyClickHandlers();
 }
+
+
 
